c55082db8711693ef813373b433d38b555cfda0f
trx40 <trm2468@gmail.com>
2023-03-29 21:23:43 +0530

    Initial commit

---
+++ src/gateway/server.py
@@ -0,0 +1,16 @@
+import os
+import gridfs
+import pika
+import json
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+
+server = Flask(__name__)
+server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
+
+mongo = PyMongo(server)
+
+fs = gridfs.GridFS(mongo.db)
